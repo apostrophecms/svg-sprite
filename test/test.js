9f77d7854939a13c36a22f1e52c3ed92911a8ea7
Alex Bea <abea@mailbox.org>
2021-12-13 11:26:46 -0600

    Basic module setup

---
+++ test/test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const testUtil = require('apostrophe/test-lib/test');
+
+describe('SVG Sprites', function () {
+  let apos;
+
+  this.timeout(10000);
+
+  after(async function () {
+    testUtil.destroy(apos);
+  });
+
+  it('should be a property of the apos object', async function () {
+    apos = await testUtil.create({
+      shortname: 'test-exporter',
+      testModule: true,
+      modules: {
+        '@apostrophecms/express': {
+          options: {
+            port: 4242,
+            session: { secret: 'test-the-svgs' }
+          }
+        },
+        '@apostrophecms/svg-sprites': {},
+        '@apostrophecms/svg-sprites-widget': {}
+      }
+    });
+
+    assert(apos.modules['@apostrophecms/svg-sprites']);
+    assert(apos.modules['@apostrophecms/svg-sprites-widget']);
+  });
+
+  // it('', async () => {
+  //   assert();
+  // });
+});
